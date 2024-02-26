5d702c461667797ad6cabe9c495ba2b289f05afe
MuammerSonmez <muammersonmezofficial@gmail.com>
2024-02-26 15:40:27 +0300

    MaskeTakip first commit

---
+++ Workspace/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //degiskenler();
+
+        Person person = new Person();
+        person.FirstName = "MUAMMER";
+        person.LastName = "SÖNMEZ";
+        person.DateOfBirthYear = 2004;
+        person.NationalIdentity = 27893153682;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+      
+
+        Console.ReadLine();
+    }
+
+
+
+}
